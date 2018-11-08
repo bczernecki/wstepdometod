9bc20d5a8b1cab4e156332bc22791131f6a3adce
Bartosz <nwp@amu.edu.pl>
2018-11-08 20:02:33 +0100

    typos

--- klasyfikacja/klasyfikacja2018.R
+++ klasyfikacja/klasyfikacja2018.R
@@ -6,7 +6,7 @@ kolory <-  colorRampPalette(colors = rev(c("red4","red","orange","white","lightb
 etykiety <- rev(c("ekstremalnie ciepły", "anomalnie ciepły", "bardzo ciepły", "ciepły", "lekko ciepły",
                   "normalny", "lekko chłodny" , "chłodny", "bardzo chłodny", "anomalnie chłodny", "ekstremalnie chłodny"))
 
-dane <- readxl::read_excel("dane/dane_sklasyfikowane.xls")
+dane <- readxl::read_excel("dane_sklasyfikowane.xls")
 dane <- apply(dane, 2, as.numeric)
 head(dane) # ktory wyglada tak...
 
@@ -15,12 +15,12 @@ head(dane) # ktory wyglada tak...
 
 
 par(mar=c(0.5,3,2,1), fig=c(0.05, 0.70, 0.06, 0.95)) # marginesy: + wymiary okna dla calej rysowanej figury:
-image(x = 1:13, y=1:nrow(dane), t(macierz[nrow(dane):1,-1]), xlab = "", xaxt='n', yaxt='n', col = kolory, ylab='')
+image(x = 1:13, y=1:nrow(dane), t(dane[nrow(dane):1,-1]), xlab = "", xaxt='n', yaxt='n', col = kolory, ylab='')
 axis(2, labels = dane[seq(from=1, to=nrow(dane), by=3),1], at=(nrow(dane):1)[seq(from=1, to=nrow(dane), by=3)], cex.axis=0.95)
 axis(3, labels = c("I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","ROK"), at=1:13, tick = F, padj = 1)
 abline(v=1.5:13, lwd=0.8)
 abline(v=12.5:13.5, lwd=2.5, lty=2, col="black") # oddzielamy serie roczna
-abline(h=1.5:nrow(macierz), lwd=0.5)
+abline(h=1.5:nrow(dane), lwd=0.5)
 box(lwd=2)
 
 # i opcjonalne dorysowanie legendy:
